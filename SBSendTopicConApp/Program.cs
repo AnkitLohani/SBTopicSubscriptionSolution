d05a3caa9f7ec3f61a075db5c73bff346d64899d
ankitlohani066@gmail.com <ankitlohani066@gmail.com>
2019-11-11 09:00:16 +0000

    Project added for Service bus Topic send receive

---
+++ SBSendTopicConApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBSendTopicConApp
+{
+    class Program
+    {
+        static NamespaceManager _namespacemanager;
+        static void Main(string[] args)
+        {
+            CollectSBDetails();
+            SendMessage();
+         //   Console.WriteLine("All Message Sent successfully....");
+        }
+
+        static void SendMessage()
+        {
+            TokenProvider tokenProvider = _namespacemanager.Settings.TokenProvider;
+            if (!_namespacemanager.TopicExists("DataCollectionTopic"))
+            {
+                _namespacemanager.CreateTopic("DataCollectionTopic");
+                if (!_namespacemanager.SubscriptionExists("DataCollectionTopic","Inventory"))
+                {
+                    _namespacemanager.CreateSubscription("DataCollectionTopic", "Inventory");
+                }
+                if (!_namespacemanager.SubscriptionExists("DataCollectionTopic", "Dashboard"))
+                {
+                    _namespacemanager.CreateSubscription("DataCollectionTopic", "Dashboard");
+                }
+                if (!_namespacemanager.SubscriptionExists("DataCollectionTopic", "Sales"))
+                {
+                    _namespacemanager.CreateSubscription("DataCollectionTopic", "Sales");
+                }
+                if (!_namespacemanager.SubscriptionExists("DataCollectionTopic", "Feedback"))
+                {
+                    _namespacemanager.CreateSubscription("DataCollectionTopic", "Feedback");
+                }
+
+                MessagingFactory factory = MessagingFactory.Create(_namespacemanager.Address, tokenProvider);
+                BrokeredMessage message = new BrokeredMessage();//can pass a user defined class
+                message.Label = "SalesReport";
+                message.Properties["StoreName"] = "Nike";
+                message.Properties["MachineID"] = "POS1";
+
+                BrokeredMessage message1 = new BrokeredMessage();//can pass a user defined class
+                message1.Label = "SalesReport";
+                message1.Properties["StoreName"] = "Adidas";
+                message1.Properties["MachineID"] = "POS3";
+
+                MessageSender sender = factory.CreateMessageSender("DataCollectionTopic");
+                sender.Send(message);
+                sender.Send(message1);
+                Console.WriteLine("Message Send Successfully");
+            }
+        }
+        
+
+        static void CollectSBDetails()
+        {
+            _namespacemanager = NamespaceManager.CreateFromConnectionString(ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"].ToString());
+        }
+    }
+
+}
---
+++ SBTopicReceiveConApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBTopicReceiveConApp
+{
+    class Program
+    {
+        static NamespaceManager _namespaceManager;
+        static void Main(string[] args)
+        {
+            CollectSBDetails();
+            ReceiveMessage();
+            Console.WriteLine($"Done:{DateTime.Now}");
+        }
+      
+        static void ReceiveMessage()
+        {
+            TokenProvider tokenProvider = _namespaceManager.Settings.TokenProvider;
+            if (_namespaceManager.TopicExists("DataCollectionTopic"))
+            {
+                MessagingFactory factory = MessagingFactory.Create(_namespaceManager.Address, tokenProvider);
+                //MessageReceiver receiver = factory.CreateMessageReceiver("DataCollectionTopic/subscriptions/Inventory");
+                MessageReceiver receiver = factory.CreateMessageReceiver("DataCollectionTopic/subscriptions/Dashboard");
+                BrokeredMessage receivedMessage = null;
+                try
+                {
+                   while ((receivedMessage=receiver.Receive())!=null)
+                    {
+                        ProcessMessage(receivedMessage);
+                        receivedMessage.Complete();
+                    }
+                    factory.Close();
+                    _namespaceManager.DeleteSubscription("DataCollectionTopic", "Inventory");
+                   // _namespaceManager.DeleteTopic("DataCollectionTopic");
+                }
+                
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.ToString());
+                    receivedMessage.Abandon();
+                }
+            }
+        }
+
+        static void ProcessMessage(BrokeredMessage receivedMessage)
+        {
+            Console.WriteLine($"Lable: {receivedMessage.Label} ,MessageID:{receivedMessage.MessageId}, StoreName :{receivedMessage.Properties["StoreName"]} and MachineID :{receivedMessage.Properties["MachineID"]}");
+        }
+
+        static void CollectSBDetails()
+        {
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"].ToString());
+        }
+    }
+}
